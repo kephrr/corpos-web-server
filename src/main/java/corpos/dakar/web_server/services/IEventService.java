0f29b29443ad486e17e1473763f1293bade8b5e1
Kephren NZE <kephrennze@gmail.com>
2025-04-01 02:20:25 +0000

    add: dashboard stats feature

--- src/main/java/corpos/dakar/web_server/services/IEventService.java
+++ src/main/java/corpos/dakar/web_server/services/IEventService.java
@@ -3,5 +3,5 @@ package corpos.dakar.web_server.services;
 import corpos.dakar.web_server.data.entites.Event;
 
 public interface IEventService extends IServiceCore<Event, Long> {
-
+// Permettre de changer l'etat en une seule requete
 }
